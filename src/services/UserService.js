b1a827baeab8679df02b8ec561ab111e01ad73d3
andrey-kupets <andrey.kupets@i.ua>
2021-05-19 20:24:50 +0300

    cors at back is fixed

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,5 @@
 export const Userservice = () => {
-    // const baseUrl = 'http://localhost:5000/users';
-    const baseUrl = 'https://jsonplaceholder.typicode.com/users';
+    const baseUrl = 'http://localhost:5000/users';
     return fetch (baseUrl)
-        .then(r => r.json())
-}
+        .then(r => r.json());
+};

680a0eaaac502d9bea2d29130327e38f3d80ac6c
andrey-kupets <andrey.kupets@i.ua>
2021-06-02 20:42:05 +0300

    practicing the post method

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,5 +1,50 @@
+// export const Userservice = () => {
+//     const baseUrl = 'http://localhost:5000/users';
+//     return fetch (baseUrl)
+//         .then(r => r.json());
+// };
+
+
 export const Userservice = () => {
     const baseUrl = 'http://localhost:5000/users';
-    return fetch (baseUrl)
-        .then(r => r.json());
+    return fetch (baseUrl, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+          {
+              name: "max3",
+              email: "max@gmail.com",
+              password: "max111",
+              _products: [
+                  {
+                      _id: "60b231505d469ae5fefc2df0"
+
+                  },
+                  {
+                      _id: "60b231505d469ae5fefc2df1"
+                  },
+                  {
+                      _id: "60b231505d469ae5fefc2df1"
+                  }
+              ]
+          }
+        )
+    })
+      .then(r => r.json());
 };
+// (async () => {
+//     const rawResponse = await fetch('https://httpbin.org/post', {
+//         method: 'POST',
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({a: 1, b: 'Textual content'})
+//     });
+//     const content = await rawResponse.json();
+//
+//     console.log(content);
+// })();

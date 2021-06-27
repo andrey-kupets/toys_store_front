b402cd89e5d95a096e0413f07f85f90d0bc48166
andrey-kupets <andrey.kupets@i.ua>
2021-06-27 13:01:16 +0300

    create user through axios.post method - template

--- src/App.js
+++ src/App.js
@@ -2,16 +2,41 @@ import './App.css';
 import {HeaderBlock} from "./components/header/HeaderBlock";
 import {MainBlock} from "./components/main/MainBlock";
 import {FooterBlock} from "./components/footer/FooterBlock";
-import {UserService} from "./services/UserService";
 import {useEffect} from "react";
-import {productService} from "./services";
+import {productService, userService} from "./services";
 
+const getUsers = async () => {
+    const users = await userService.getUsers();
+    console.log(users);
+}
+
+const getUserById = async (userId) => {
+  const user = await userService.getUserById(userId);
+  console.log(user);
+}
+
+const createUser = async () => {
+  const createdUserResponse = await userService.createUser();
+  console.log(createdUserResponse);
+}
+
+const getProducts = async () => {
+  const products = await productService.getProducts();
+  console.log(products);
+}
+
+const getProductById = async (productId) => {
+  const product = await productService.getProductById(productId);
+  console.log(product);
+}
 function App() {
-    useEffect(() => {
-        // UserService().then(r => console.log(r));
-        productService.getProducts().then(console.log);
-        productService.getProductById("60b231505d469ae5fefc2de8").then(r => console.log(r));
-    }, [])
+  useEffect(() => {
+    createUser();
+    getUsers();
+    getUserById("60b229625d469ae5fefc2ddc");
+    getProducts();
+    getProductById("60b231505d469ae5fefc2de8");
+  }, [])
 
   return (
     <div className="App">
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,49 +1,37 @@
-// export const Userservice = () => {
-//     const baseUrl = 'http://localhost:5000/users';
-//     return fetch (baseUrl)
-//         .then(r => r.json());
-// };
+import { axiosDB } from './axiosConfig';
 
-export const UserService = () => {
-    const baseUrl = 'http://localhost:5000/users';
-    return fetch(baseUrl, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+class UserService {
+  async getUsers() {
+    const { data } = await axiosDB('/users'); // get by default
+    return data;
+  }
+
+  async getUserById(userId) {
+    const { data } = await axiosDB(`/users/${userId}`); // get by default
+    return data;
+  }
+
+  async createUser() {
+    const { data } = await axiosDB.post('/users', {
+      name: "max",
+      email: "max@gmail.com",
+      password: "max111",
+      _products: [
+        {
+          _id: "60b231505d469ae5fefc2df0"
+
         },
-        body: JSON.stringify(
-          {
-              name: "max3",
-              email: "max@gmail.com",
-              password: "max111",
-              _products: [
-                  {
-                      _id: "60b231505d469ae5fefc2df0"
+        {
+          _id: "60b231505d469ae5fefc2df1"
+        },
+        {
+          _id: "60b231505d469ae5fefc2df1"
+        }
+      ]
+    });
 
-                  },
-                  {
-                      _id: "60b231505d469ae5fefc2df1"
-                  },
-                  {
-                      _id: "60b231505d469ae5fefc2df1"
-                  }
-              ]
-          }
-        )
-    })
-      .then((r) => r.json());
-};
-// (async () => {
-//     const rawResponse = await fetch('https://httpbin.org/post', {
-//         method: 'POST',
-//         headers: {
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({a: 1, b: 'Textual content'})
-//     });
-//     const content = await rawResponse.json();
-//
-//     console.log(content);
-// })();
+    return data;
+  }
+}
+
+export const userService = new UserService();

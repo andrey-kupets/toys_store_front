33060231192a6c04d101f6f474439ba40474ba00
andrey-kupets <andrey.kupets@i.ua>
2021-07-01 19:55:30 +0300

    add last styles for today for ProductsList.module.css & ProductItem.module.css

--- src/components/main/product-item/ProductItem.js
+++ src/components/main/product-item/ProductItem.js
@@ -2,14 +2,6 @@ import React from "react";
 import styles from './ProductItem.module.css';
 
 export const ProductItem = (props) => {
-  // category: "Игрушки для малышей"
-  // description: "- яркая погремушка, благодаря тонкому звуку которой ребенок развивает осязательные, зрительные и двигательные способности, учит причинно-следственные связи."
-  // id: "60db7976668005d41cce29a3"
-  // img: "https://pampik.com/uploads/product/small/20171212/20171212_1lWT.jpg"
-  // name: "Погремушка BabyOno Шарик (672)"
-  // price: 42
-  // type: "Погремушки"
-  // _id: "60db7976668005d41cce29a3"
   const {
     category,
     description,
@@ -21,15 +13,26 @@ export const ProductItem = (props) => {
 
   return (
     <div className={styles.product_item}>
+      {/*may so*/}
+      {/*<div*/}
+      {/*  style={{*/}
+      {/*    backgroundRepeat: 'no-repeat',*/}
+      {/*    backgroundSize: '100%',*/}
+      {/*    backgroundPosition: 'center',*/}
+      {/*    height: 300,*/}
+      {/*    width: '100%',*/}
+      {/*    backgroundImage: `url(${img})`*/}
+      {/*  }}>*/}
+      {/*</div>*/}
       <div className={styles.product_image_wrapper}>
         <img className={styles.product_image} src={img} alt={`${name} toy`}/>
       </div>
       <div>
         <h3>{name}</h3>
-        <div>Price: {price} грн.</div>
-        <div>Category: {category}</div>
-        <div>Type: {type}</div>
-        <p>{description}</p>
+        <span>Price: {price} грн.</span><br/>
+        {/*<span>Category: {category}</span><br/>*/}
+        {/*<span>Type: {type}</span>*/}
+        <p className={styles.product_item_description}>{description}</p>
       </div>
     </div>
   )

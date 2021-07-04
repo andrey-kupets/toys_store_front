6f194ac31e5a0fdee23e1cd3287fda0c05767274
andrey-kupets <andrey.kupets@i.ua>
2021-07-04 23:35:09 +0300

    refactor logo

--- src/components/header/HeaderBlock.js
+++ src/components/header/HeaderBlock.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from './HeaderBlock.module.css';
-import { Logo } from "./logo";
+import { Logo } from "../logo";
 import { constants } from '../../constants';
 import { Link } from "react-router-dom";
 import { Wishlist } from "../wishlist";
@@ -8,23 +8,21 @@ import { Cart } from "../cart";
 
 export const HeaderBlock = () => {
   return (
-    <div className={styles.header}>
-      <div className={styles.header_wrapper}>
-        <Logo/>
-        <div><Link to='/products'>Главная</Link></div>
-        <div>Контакты</div>
-        <div>
-          <label>Поиск</label>
-          <input
-            className={styles.header_input} type="text"
-            placeholder='Название продукта целиком / часть фразы'
-          />
-        </div>
-        <div>Вход</div>
-        <div className={styles.cart_wishlist_block}>
-          <Wishlist title={'Отложенные'}/>
-          <Cart title={'Корзина'}/>
-        </div>
+    <div className={styles.header_wrapper}>
+      <Logo/>
+      <div><Link to='/products'>Главная</Link></div>
+      <div>Контакты</div>
+      <div>
+        <label>Поиск</label>
+        <input
+          className={styles.header_input} type="text"
+          placeholder='Название продукта целиком / часть фразы'
+        />
+      </div>
+      <div>Вход</div>
+      <div className={styles.cart_wishlist_block}>
+        <Wishlist title={'Отложенные'}/>
+        <Cart title={'Корзина'}/>
       </div>
     </div>
   );
--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { constants } from '../../../constants/constants';
-import styles from '../HeaderBlock.module.css';
+import { constants } from '../../constants';
+import styles from './Logo.module.css';
 
 export const Logo = () => ( // early return pattern
   <div>

d1869498a433a32778329224acdb064dd2b42f9e
andrey-kupets <andrey.kupets@i.ua>
2021-07-08 21:29:47 +0300

    input actions

--- src/components/left-sidebar/LeftSideBar.js
+++ src/components/left-sidebar/LeftSideBar.js
@@ -1,29 +1,40 @@
 import React from "react";
 import styles from './LeftSideBar.module.css';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { constants } from '../../constants';
+
 
 export const LeftSideBar = () => {
+    const history = useHistory();
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        const priceGte = e.target[0].value;
+        const priceLte = e.target[1].value;
+
+        history.push(`/products?gte=${priceGte}&lte=${priceLte}`);
+    }
+
     return (
         <div className={styles.left_sideBar}>
             <div className={styles.left_sideBar_category}>
                 <h1>Категория</h1>
                 <ul>
-                    {/*<li><a href="1">Игрушки для малышей</a></li>*/}
-                    <li><Link to="/products?category=Игрушки%20для%20малышей">Игрушки для малышей</Link></li>
-                    <li><a href="1">Машинки и спецтехника</a></li>
-                    <li><a href="1">Коляски и куклы</a></li>
-                    <li><a href="1">Творчество</a></li>
-                    <li><a href="1">Развивающие игрушки</a></li>
-                    <li><a href="1">Мягкие игрушки</a></li>
-                    <li><a href="1">Настольные игры</a></li>
+                    <li><Link to={`products?category=${constants.toys_for_toddlers}`}>Игрушки для малышей</Link></li>
+                    <li><Link to={`/products?category=${constants.cars_and_special_equipment}`}>Машинки и спецтехника</Link></li>
+                    <li><Link to={`/products?category=${constants.strollers_and_dolls}`}>Коляски и куклы</Link></li>
+                    <li><Link to={`/products?category=${constants.creation}`}>Творчество</Link></li>
+                    <li><Link to={`/products?category=${constants.educational_toys}`}>Развивающие игрушки</Link></li>
+                    <li><Link to={`/products?category=${constants.stuffed_toys}`}>Мягкие игрушки</Link></li>
+                    <li><Link to={`/products?category=${constants.board_games}`}>Настольные игры</Link></li>
                 </ul>
             </div>
             <div className={styles.left_sideBar_filter}>
                 <h3>Ценовой фильтр</h3>
-                <div>
-                    <input type="text" placeholder='Укажите цену "От" ...грн.'/><br/>
-                    <input type="text" placeholder='Укажите цену "До" ...грн.'/>
-                </div>
+                <form onSubmit={onFormSubmit}>
+                    <input type="number" placeholder='Укажите цену "От" ...грн.'/><br/>
+                    <input type="number" placeholder='Укажите цену "До" ...грн.'/>
+                    <button>Submit</button>
+                </form>
             </div>
         </div>
     );

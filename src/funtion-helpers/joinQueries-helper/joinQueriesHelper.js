eeeefa6c5028f1a0b5a5ab53d795bf5a81088d4f
andrey-kupets <andrey.kupets@i.ua>
2021-07-14 23:52:46 +0300

    rewrite transformQuery func on map

--- src/funtion-helpers/joinQueries-helper/joinQueriesHelper.js
+++ src/funtion-helpers/joinQueries-helper/joinQueriesHelper.js
@@ -2,15 +2,21 @@ export const transformQuery = (searchParams, newQuery) => {
   const currentQuery = {};
   const currentQueryArray = searchParams.split('&');
 
-  currentQueryArray.forEach(el => {
-    const items = el.split('=');
 
-    for (let i = 0; i < items.length; i++) {
-      const item = items[i];
+  currentQueryArray.map(el => el.split('=').map((item, i) => {
+      if (!(i % 2)) currentQuery[item] = el.split('=')[i + 1]
+    })
+  );
 
-      if (!(i % 2)) currentQuery[item] = items[i + 1];
-    }
-  })
+  // currentQueryArray.forEach(el => {
+  //   const items = el.split('=');
+  //
+  //   for (let i = 0; i < items.length; i++) {
+  //     const item = items[i];
+  //
+  //     if (!(i % 2)) currentQuery[item] = items[i + 1];
+  //   }
+  // })
 
   const newQueryObj = !!searchParams ? { ...currentQuery, ...newQuery } :{ ...newQuery };
   return Object.entries(newQueryObj).map(el => el.join('=')).join('&');

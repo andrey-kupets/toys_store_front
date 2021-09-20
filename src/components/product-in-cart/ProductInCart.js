42c69e754ad79410c1d9d6d783633fb0b256bda7
WIN-10LITE <andrey.kupets1983@gmail.com>
2021-09-21 00:29:34 +0300

    add previous Cart rendering inside: space-between

--- src/components/product-in-cart/ProductInCart.js
+++ src/components/product-in-cart/ProductInCart.js
@@ -1,19 +1,18 @@
 import React from "react";
 import styles from './ProductInCart.module.css';
 
-export const ProductInCart = ({ img, name, price, item: { count }}) => {
+export const ProductInCart = ({ img, name, price, item: { count } }) => {
   return (
     <div className={styles.product_card_wrapper}>
-        <div>
-          <img className={styles.product_card_image} src={img}/>
-        </div>
-        <div className={styles.product_card_info}>
-          <div className={styles.product_card_name}>{ name }</div>
-          {/*<div>*/}
-            <div>{ price }</div>
-            <div>{ count }</div>
-          </div>
-        {/*</div>*/}
+      <div>
+        <img className={styles.product_card_image} src={img}/>
+      </div>
+      <div className={styles.product_card_info}>
+        <div className={styles.product_card_name}>{name}</div>
+        <div>{price}</div>
+        <div>{count}</div>
+        <div>{price*count}</div>
+      </div>
     </div>
   );
 };

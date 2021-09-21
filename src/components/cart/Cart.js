217422d001b4191b94dd6aacc8788233b1551804
WIN-10LITE <andrey.kupets1983@gmail.com>
2021-09-22 00:55:35 +0300

    add hr to order_modal

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -40,6 +40,7 @@ export const Cart = () => {
         <span><b>ИТОГО</b> с доставкой <b>{ sumTotals < 500 ? sumTotals + 50 : sumTotals }</b> грн.</span>
         <button className={styles.order_modal_button}>ОФОРМИТЬ ЗАКАЗ</button>
         <span className={styles.center}>Стоимость доставки вы сможете узнать в разделе "Условия оплаты и доставки"</span>
+        <hr/>
         <button className={styles.order_modal_button}>ОЧИСТИТЬ КОРЗИНУ</button>
       </div>
     </div>

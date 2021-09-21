0791a743cd16cafb94eac8dd283a61c678c5f20e
WIN-10LITE <andrey.kupets1983@gmail.com>
2021-09-21 20:52:13 +0300

    add Cart rendering: previous order_modal

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import styles from './Cart.module.css';
 import { useDispatch, useSelector } from "react-redux";
 import { ProductInCart } from "../product-in-cart";
@@ -26,36 +26,17 @@ export const Cart = () => {
   console.log(user)
 
   return (
-    <div className={styles.cart_wrapper}>
-      {!!user?._productsInCart.length
-        ? user._productsInCart
-          .map(el => <ProductInCart key={el._id} {...el} item={user?._cart.find((item) => item._id === el._id)}/>)
-        : 'ЗДЕСЬ МОГУТ БЫТЬ ВАШИ ПРОДУКТЫ'
-      }
+    <div className={styles.flex}>
+      <div className={styles.product_cards_wrapper}>
+        {!!user?._productsInCart.length
+          ? user._productsInCart
+            .map(el => <ProductInCart key={el._id} {...el} item={user?._cart.find((item) => item._id === el._id)}/>)
+          : 'ЗДЕСЬ МОГУТ БЫТЬ ВАШИ ПРОДУКТЫ'
+        }
+      </div>
+      <div className={styles.order_modal_wrapper}>
+        Всего 16 товаров на сумму 8 918 грн.
+      </div>
     </div>
   );
 };
-
-
-// <div>
-//   <span>Стоимость товара ИТОГО:</span>
-//   <hr/>
-//   <div className={styles.cart_titles}>
-//     <span>Товар</span>
-//     <span>Цена</span>
-//     <span>Количество</span>
-//     <span>Сумма</span>
-//   </div>
-//   <div className={styles.cart_payload}>
-//     <div>product.image</div>
-//     <div>product.name</div>
-//     <div>product.price грн.</div>
-//     <div>Количество
-//       <button>-</button>
-//       <span>1</span>
-//       <button>+</button>
-//     </div>
-//     <div>Сумма{}грн.</div>
-//   </div>
-// </div>
-
